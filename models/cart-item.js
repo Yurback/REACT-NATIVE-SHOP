f530c56b2fd1c2188c397e449f706c4b6e604a15
Yurback <ryv1983@mail.ru>
2021-05-22 19:16:53 +0300

    font set up

---
+++ models/cart-item.js
@@ -0,0 +1,10 @@
+class CartItem {
+    constracror(quantity, productPrice, productTitle, sum) {
+        this.quantity = quantity;
+        this.productPrice = productPrice;
+        this.productTitle = productTitle;
+        this.sum = sum
+    }
+};
+
+export default CartItem;
